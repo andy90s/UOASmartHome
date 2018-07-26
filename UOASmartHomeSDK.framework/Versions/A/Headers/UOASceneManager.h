715d063b526c03025878b0ae607dd11f445f89af
梁先华 <909901234@qq.com>
2018-07-26 17:17:23 +0800

    0.5.9

--- UOASmartHomeSDK.framework/Versions/A/Headers/UOASceneManager.h
+++ UOASmartHomeSDK.framework/Versions/A/Headers/UOASceneManager.h
@@ -30,9 +30,48 @@ typedef void(^failer)(NSDictionary *jsonDictionary);
  @param results 结果条件
  @param recResults 执行条件
  */
-- (void)addSceneWithName:(NSString *)name conditions:(NSDictionary *)conditions results:(NSDictionary *)results recResults:(NSDictionary *)recResults;
+- (void)addSceneWithName:(NSString *)name
+              conditions:(NSDictionary *)conditions
+                 results:(NSDictionary *)results
+              recResults:(NSDictionary *)recResults;
 
 - (void)addSceneWithName:(NSString *)name conditions:(NSDictionary *)conditions results:(NSDictionary *)results recResults:(NSDictionary *)recResults callBack:(success)success failer:(failer)failer;
+
+/**
+ 删除场景
+
+ @param sceneid 场景ID
+ */
+- (void)deleteSceneWithID:(NSInteger )sceneid;
+
+- (void)deleteSceneWithID:(NSInteger )sceneid callBack:(success)success failer:(failer)failer;
+
+/**
+ 控制场景
+
+ @param sceneid 场景ID
+ @param onoff 开关
+ */
+- (void)controlSceneWithID:(NSInteger )sceneid onoff:(NSInteger )onoff;
+
+- (void)controlSceneWithID:(NSInteger )sceneid onoff:(NSInteger )onoff callBack:(success)success failer:(failer)failer;
+
+/**
+ 修改场景
+
+ @param name 场景名称
+ @param sceneid 场景ID
+ @param conditions 启动条件
+ @param results 结束条件
+ @param recResults 执行条件
+ */
+- (void)editSceneWithName:(NSString *)name
+                  sceneid:(NSInteger )sceneid
+               conditions:(NSDictionary *)conditions
+                  results:(NSDictionary *)results
+               recResults:(NSDictionary *)recResults;
+
+- (void)editSceneWithName:(NSString *)name sceneid:(NSInteger )sceneid conditions:(NSDictionary *)conditions results:(NSDictionary *)results recResults:(NSDictionary *)recResults callBack:(success)success failer:(failer)failer;
 
 @end
 NS_ASSUME_NONNULL_END
